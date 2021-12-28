eab00f37cbba79a1a95bb7bd80b0a843f3910f9e
fufeng <fu__feng@163.com>
2021-12-29 03:22:43 +0800

    fix bug

--- contracts/SlimeTokenbase.sol
+++ contracts/SlimeTokenbase.sol
@@ -3,7 +3,6 @@
 pragma solidity 0.8.0;
 
 import "./Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
 
 abstract contract SlimeTokenbase is ERC20Pausable, Ownable {
