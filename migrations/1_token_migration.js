9da7e7df5327b81aa22c0e5d4bf254c7bbd69aff
fufeng <fufeng@badambiz.com>
2021-12-30 20:11:24 +0800

    11

--- migrations/1_token_migration.js
+++ migrations/1_token_migration.js
@@ -1,11 +1,11 @@
 
 var SlimeCoin = artifacts.require("SlimeCoin");
-var SlimeNFT = artifacts.require("SlimeNFT");
+var SlimeTPS = artifacts.require("SlimeTokenPrivateSale");
 
 module.exports = function(deployer) {
     deployer.deploy(SlimeCoin)
         // 等待、直到合约部署完成
         .then(() => SlimeCoin.deployed())
         // 传递 Storage 合约地址，部署 InfoManager 合约
-        .then(() => deployer.deploy(SlimeNFT, SlimeCoin.address));
+        .then(() => deployer.deploy(SlimeTPS, SlimeCoin.address));
 }
--- others/测试脚本.js
+++ others/测试脚本.js
@@ -1,2 +1,5 @@
 SlimeNFT.deployed().then(function(instance){n=instance})
 SlimeCoin.deployed().then(function(instance){c=instance})
+SlimeTokenPrivateSale.deployed().then(function(instance){t=instance})
+
+var ac = web3.eth.getAccounts()
